fbf4d3a9970ca78fb0f276884ccbf9047333d748
krzszk96 <krzszk96@gmail.com>
2019-12-19 23:06:15 +0100

    menu desing added

--- app/src/main/java/com/example/bookanyhallcom/MainActivity.java
+++ app/src/main/java/com/example/bookanyhallcom/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.bookanyhallcom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,35 +20,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addHall();
-        search();
-
-
-
+//        addHall();
+//        search();
     }
-
-
-
-    private void addHall(){
-        Button btnaddHall = (Button) findViewById(R.id.btnaddHall);
-        btnaddHall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AddHall.class));
-            }
-        });
-
-    }
-
-    private void search(){
-        Button btnsearch = (Button) findViewById(R.id.btnsearch);
-        btnsearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Search.class));
-            }
-        });
-
-    }
+//
+//    private void addHall(){
+//        ImageView btnaddHall = (ImageView) findViewById(R.id.btnaddHall);
+//        btnaddHall.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, AddHall.class));
+//            }
+//        });
+//
+//    }
+//
+//    private void search(){
+//        ImageView btnsearch = (ImageView) findViewById(R.id.btnsearch);
+//        btnsearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, Search.class));
+//            }
+//        });
+//
+//    }
 
 }
