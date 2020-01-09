f59f6bae7f77c6f13536141a9eadaf290da8db98
krzysztof <krzszk96@gmail.com>
2020-01-09 01:17:10 +0100

    database addd, view

--- app/src/main/java/com/example/bookanyhallcom/AddHall.java
+++ app/src/main/java/com/example/bookanyhallcom/AddHall.java
@@ -19,37 +19,49 @@ public class AddHall extends AppCompatActivity {
     Button btnviewAll;
     Intent intent;
 
-    private static String value;
-    public static String getValue() {
-        return value;
-    }
+//    private static String value,value1, value2, value3;
+//
+//    public static String getValue() {
+//        return value;
+//    }
+//    public static String getValue1() {
+//        return value1;
+//    }
+//    public static String getValue2() {
+//        return value2;
+//    }
+//    public static String getValue3() {
+//        return value3;
+//    }
+//
+//    @Override
+//    protected void onCreate (Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_add_hall2);
+//
+//        backToMain1();
+//
+//        editName = findViewById(R.id.hname);
+//        editStreet = findViewById(R.id.hstreet);
+//        editPost = findViewById(R.id.hpost);
+//        editCity = findViewById(R.id.hcity);
+//
+//        Button button = findViewById(R.id.addhallbtn);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                value = editName.getText().toString().trim();
+//                value1 = editStreet.getText().toString().trim();
+//                value2 = editPost.getText().toString().trim();
+//                value3 = editCity.getText().toString().trim();
+//                Intent intent = new Intent(AddHall.this, Search.class);
+//                startActivity(intent);
+//            }
+//
+//        });
+//    }
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_hall2);
-
-        backToMain1();
-
-        final EditText editText = findViewById(R.id.hname);
-        /*final EditText editText1 = findViewById(R.id.hstreet);
-        final EditText editText2 = findViewById(R.id.hpost);
-        final EditText editText3 = findViewById(R.id.hcity);*/
-        Button button = findViewById(R.id.addhallbtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value = editText.getText().toString().trim();
-                /*value = editText1.getText().toString().trim();
-                value = editText2.getText().toString().trim();
-                value = editText3.getText().toString().trim();*/
-                Intent intent = new Intent(AddHall.this, Search.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_hall2);
@@ -67,7 +79,7 @@ public class AddHall extends AppCompatActivity {
 
         AddData();
         viewAll();
-    }*/
+    }
 
       private void backToMain1(){
         Button backbtn = (Button) findViewById(R.id.backbtn1);
@@ -79,7 +91,7 @@ public class AddHall extends AppCompatActivity {
         });
     }
 
-    /*public void AddData(){
+    public void AddData(){
         btnadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -102,6 +114,9 @@ public class AddHall extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        Intent intent = new Intent(AddHall.this, Search.class);
+                        startActivity(intent);
+
                         Cursor res =  myDB.getAllData();
                         if(res.getCount() == 0){
                             //show message
@@ -118,6 +133,7 @@ public class AddHall extends AppCompatActivity {
                         }
                         //show all data
                         showMessage("Data", buffer.toString());
+
                     }
                 }
         );
@@ -129,7 +145,7 @@ public class AddHall extends AppCompatActivity {
         builder.setTitle(title);
         builder.setMessage(Message);
         builder.show();
-    }*/
+    }
 
 
 }
--- app/src/main/java/com/example/bookanyhallcom/Search.java
+++ app/src/main/java/com/example/bookanyhallcom/Search.java
@@ -2,32 +2,58 @@ package com.example.bookanyhallcom;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
 
-    TextView receive,receive1,receive2,receive3;
+    DatabaseHelper myDB;
+    ListView listView;
+    //TextView receive,receive1,receive2,receive3;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        receive = findViewById(R.id.tn);
-        receive.setText(AddHall.getValue());
-        receive1 = findViewById(R.id.ts);
-        receive1.setText(AddHall.getValue1());
-        TextView receive2 = findViewById(R.id.tp);
-        receive2.setText(AddHall.getValue2());
-        TextView receive3 = findViewById(R.id.tc);
-        receive3.setText(AddHall.getValue3());
+//        receive = findViewById(R.id.tn);
+//        receive.setText(AddHall.getValue());
+//        receive1 = findViewById(R.id.ts);
+//        receive1.setText(AddHall.getValue1());
+//        receive2 = findViewById(R.id.tp);
+//        receive2.setText(AddHall.getValue2());
+//        receive3 = findViewById(R.id.tc);
+//        receive3.setText(AddHall.getValue3());
 
         /*@Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);*/
+
+        //ListView listView = (ListView) findViewById(R.id.ListView);
+        myDB = new DatabaseHelper(this);
+
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor data = myDB.getAllData();
+
+        if(data.getCount() == 0){
+            Toast.makeText(Search.this, "Database empty", Toast.LENGTH_LONG).show();
+        }else{
+            while(data.moveToNext()){
+                theList.add(data.getString(0));
+                theList.add(data.getString(1));
+                ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,theList);
+                listView.setAdapter(listAdapter);
+            }
+        }
 
         backToMain2();
     }
